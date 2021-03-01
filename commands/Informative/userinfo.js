e4a33af92327ecf4a885bf8029eb1d6e10778437
piyerik <mddamboiu@gmail.com>
2021-03-01 16:19:20 -0500

    Just a teeny change, joined the array with ", "

--- commands/Informative/userinfo.js
+++ commands/Informative/userinfo.js
@@ -6,46 +6,63 @@ module.exports = {
     description: 'Shows informated related to a user',
     usage: 'userinfo <user>',
     aliases: ['memberinfo', 'whois'],
-    async execute(client, message, args) {    
+    async execute(client, message, args) {
         var member;
 
-    function getUserFromMention(mention) {
-        if (!mention) return false;
-        const matches = mention.match(/^<@!?(\d+)>$/);
-        if (!matches) return mention;
-        const id = matches[1];
+        function getUserFromMention(mention) {
+            if (!mention) return false;
+            const matches = mention.match(/^<@!?(\d+)>$/);
+            if (!matches) return mention;
+            const id = matches[1];
 
-        return id;
-    }
+            return id;
+        }
 
-    try {
-        member = await message.guild.members.cache.find(member => member.id == parseInt(getUserFromMention(args[0])));
-    } catch (err) {
-        member = null
-    }
-    if (!member) member = message.member
+        try {
+            member = await message.guild.members.cache.find(member => member.id == parseInt(getUserFromMention(args[0])));
+        } catch (err) {
+            member = null
+        }
+        if (!member) member = message.member
 
-    let memberRoles = new Array();
-    member.roles.cache.forEach(role => {
-        memberRoles.push("<@&" + role.id + ">")
-    })
-    memberRoles.pop(0)
+        let memberRoles = new Array();
+        member.roles.cache.forEach(role => {
+            memberRoles.push("<@&" + role.id + ">")
+        })
+        memberRoles.pop(0)
 
-    if (memberRoles.length < 1) memberRoles = "No roles";
+        if (memberRoles.length < 1) memberRoles = "No roles";
 
-    const userinfo = new Discord.MessageEmbed()
-    .setColor('#09fff2')
-    .addField('Username', member.user.username, true)
-    .addField('ID', member.id, true)
-    .addField('Bot Account?', member.user.bot, true, true)
-    .addField('Joined on', moment(member.joinedAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), true)
-    .addField('Account Creation', moment(member.user.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), true)
-    .addField(`Roles [${member.roles.cache.size - 1}]`, memberRoles.join(', '))
-    .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
-    .setAuthor(`User Info - ${member.user.tag}`, member.user.displayAvatarURL())
-    .setFooter(`Information requested by ${message.author.tag}`)
-    .setThumbnail(member.user.displayAvatarURL({size: 1024}));
+        var bot;
+        if (member.user.bot) {
+            bot = 'Yes'
+        } else {
+            bot = 'No'
+        }
 
-    message.channel.send(userinfo);
+        var status;
+        if (member.presence.status === 'dnd') {
+            status = 'Do Not Disturb'
+        } else if (member.presence.status === 'idle') {
+            status = 'Idle'
+        } else if (member.presence.status === 'online') {
+            status = 'Online'
+        } else {
+            status = 'Offline'
+        }
+
+        const userinfo = new Discord.MessageEmbed()
+            .setColor('#09fff2')
+            .setDescription(`User information for ${member.user} (${member.user.username})`)
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+            .addField('User ID', member.id, false)
+            .addField('Bot Account?', bot, true)
+            .addField('Status', status, true)
+            .addField('Joined', moment(member.joinedAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), false)
+            .addField('Created', moment(member.user.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), false)
+            .addField(`Roles [${member.roles.cache.size - 1}]`, memberRoles.join(', '), false)
+            .setFooter(`Information requested by ${message.author.tag}`)
+
+        message.channel.send(userinfo);
     }
 }
