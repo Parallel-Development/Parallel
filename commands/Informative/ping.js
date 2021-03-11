38757fbc0e12795c0509afa0d696fa73ef52d42c
piyerik <mddamboiu@gmail.com>
2021-03-11 15:51:36 -0500

    ok buddy

--- commands/Informative/ping.js
+++ commands/Informative/ping.js
@@ -6,9 +6,9 @@ module.exports = {
     usage: 'ping',
     aliases: ['pong'],
     async execute(client, message, args) {    
-        const m = await message.channel.send('Pinging...')
+        const m = await message.channel.send('Ask me if I do this everyday, I said often')
         const currentDate = new Date().getTime()
-        m.edit(`Pong! Websocket: \`${client.ws.ping}ms\``)
+        m.edit(`Pong! Websocket: \`${client.ws.ping}ms\` | Latency: \`${Date.now() - m.createdTimestamp}ms\``)
     }
 }
 
