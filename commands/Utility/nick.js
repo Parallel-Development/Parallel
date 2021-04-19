be0720585b08035e5c8c43e887046dbda47c7f1d
piyerik <mddamboiu@gmail.com>
2021-04-19 16:37:40 -0400

    s

--- commands/Utility/nick.js
+++ commands/Utility/nick.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'nick',
     description: 'Changes the nickname of the specified member to the specified nickname',
-    permisisons: 'MANAGE_NICKNAMES',
+    permissions: 'MANAGE_NICKNAMES',
     usage: 'nick <user> [New Nickname]',
     async execute(clinet, message, args) {
         function getUserFromMention(mention) {
@@ -25,6 +25,12 @@ module.exports = {
             member = null
         }
         if (!member) return message.channel.send('Please specify a valid member | The member must be on the server')
+        if(member.roles.highest.position <= message.guild.me.roles.highest.position) {
+            return message.reply('cannot change the user\'s nickname for their highest role is above my highest role')
+        }
+        if (member.roles.highest.position <= message.member.roles.highest.position) {
+            return message.reply('you cannot change the user\'s nickname for their highest role is equal or above your highest role in hierarchy')
+        }
 
         let nick = args.slice(1).join(' ')
         if(!nick) nick = null;
