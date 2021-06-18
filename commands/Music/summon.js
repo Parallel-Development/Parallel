19217a5cffb7d5ebd834700f069966d81727912c
Piyerik <70032196+Piyerik@users.noreply.github.com>
2021-06-18 17:21:22 -0400

    Update summon.js

--- commands/Music/summon.js
+++ commands/Music/summon.js
@@ -11,9 +11,9 @@ module.exports = {
             if (oldState.channelID !== oldState.guild.me.voice.channelID || newState.channel)
               return;
       
-            if (!(oldState.channel.members.filter(m => m.user.bot == false).size))
+            if (oldState.channel.members.filter(m => m.user.bot == false).size == 0)
               return setTimeout(() => { 
-                if (!(oldState.channel.members.filter(m => m.user.bot == false).size))
+                if (oldState.channel.members.filter(m => m.user.bot == false).size == 0)
                   oldState.channel.leave(); 
               }, 60000); 
           });
