ab6eab51f682c6d659e78a2f622eb0ba0f1b4161
Piyerik <70032196+Piyerik@users.noreply.github.com>
2021-06-18 16:04:32 -0400

    Update summon.js

--- commands/Music/summon.js
+++ commands/Music/summon.js
@@ -8,16 +8,14 @@ module.exports = {
     aliases: ['join', 'connect'],
     async execute(client, message, args) {
         client.on('voiceStateUpdate', (oldState, newState) => {
-            // if nobody left the channel in question, return.
             if (oldState.channelID !== oldState.guild.me.voice.channelID || newState.channel)
               return;
       
-            // otherwise, check how many people are in the channel now
-            if (!(oldState.channel.members.size - 1))
-              return setTimeout(() => { // if 1 (you), wait five minutes
-                if (!(oldState.channel.members.size - 1)) // if there's still 1 member, 
-                  oldState.channel.leave(); // leave
-              }, 60000); // (1 minute in ms)
+            if (!(oldState.channel.members.filter(m => m.user.bot == false).size))
+              return setTimeout(() => { 
+                if (!(oldState.channel.members.filter(m => m.user.bot == false).size))
+                  oldState.channel.leave(); 
+              }, 60000); 
           });
         const notinVC = new Discord.MessageEmbed()
             .setColor('#FF0000')
