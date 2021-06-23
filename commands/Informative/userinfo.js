9fd0420069ff7fe5fe6cdb2fc0795f6abda63221
Piyerik <70032196+Piyerik@users.noreply.github.com>
2021-06-23 12:26:31 -0400

    Update userinfo.js

--- commands/Informative/userinfo.js
+++ commands/Informative/userinfo.js
@@ -18,58 +18,65 @@ module.exports = {
             return id;
         }
 
+        let userNotMember = false;
+
         try {
             member = await message.guild.members.cache.find(member => member.id == parseInt(getUserFromMention(args[0])));
         } catch (err) {
-            member = null
+            member = await client.users.fetch(args[0])
+            .catch(() => { member = null })
+            userNotMember = true;
         }
         if (!member) member = message.member
+        const user = await client.users.fetch(member.id)
 
         let memberRolesRaw = new Array();
         let memberRoles;
-        member.roles.cache.forEach(role => {
-            memberRolesRaw.push("<@&" + role.id + ">")
-        })
-        memberRolesRaw.pop(0)
-        if(member.roles.cache.size < 1) {
-            memberRoles = 'None'
-        } else {
-            memberRoles = memberRolesRaw.join(', ')
+        if(!userNotMember) {
+            member.roles.cache.forEach(role => {
+                memberRolesRaw.push("<@&" + role.id + ">")
+            })
+            memberRolesRaw.pop(0)
+            if(member.roles.cache.size < 1) {
+                memberRoles = 'None'
+            } else {
+                memberRoles = memberRolesRaw.join(', ')
+            }
+
+            if (memberRoles.length < 1) memberRoles = "No roles";
         }
 
-        if (memberRoles.length < 1) memberRoles = "No roles";
-
         var bot;
-        if (member.user.bot) {
+        if (user.bot) {
             bot = 'Yes'
         } else {
             bot = 'No'
         }
 
         var status;
-        if (member.presence.status === 'dnd') {
+        if (user.presence.status === 'dnd') {
             status = 'Do Not Disturb'
-        } else if (member.presence.status === 'idle') {
+        } else if (user.presence.status === 'idle') {
             status = 'Idle'
-        } else if (member.presence.status === 'online') {
+        } else if (user.presence.status === 'online') {
             status = 'Online'
         } else {
             status = 'Offline'
         }
 
-        let description = `User information for ${member}`;
-        if(member.user.username !== member.displayName) description += ` (${member.user.username})`
+        let description = `User information for ${user}`;
+        if(!userNotMember) if(member.user.username !== member.displayName) description += ` (${member.user.username})`
 
         const userinfo = new Discord.MessageEmbed()
             .setColor('#09fff2')
             .setDescription(description)
-            .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-            .addField('User ID', member.id, false)
+            .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+            .addField('User ID', user.id, false)
             .addField('Bot Account?', bot, true)
             .addField('Status', status, true)
-            .addField('Joined', moment(member.joinedAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), false)
-            .addField('Created', moment(member.user.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), false)
-            .addField(`Roles [${member.roles.cache.size - 1}]`, memberRoles, false)
+            if(!userNotMember) userInfo.addField('Joined', moment(member.joinedAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), false)
+            userInfo.addField('Created', moment(user.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), false)
+            if(!userNotMembrr) userInfo.addField(`Roles [${member.roles.cache.size - 1}]`, memberRoles, false)
             .setFooter(`Information requested by ${message.author.tag}`, message.member.user.displayAvatarURL())
 
         message.channel.send(userinfo);
