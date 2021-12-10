767bc6bfbf22cd23405715cbdf0d4fbbcfc10fa4
Piyerik <piyeris0@gmail.com>
2021-12-09 20:39:03 -0500

    show the guild avatar by default when using /avatar and >avatar

--- SlashCommands/Information/avatar.js
+++ SlashCommands/Information/avatar.js
@@ -7,14 +7,18 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('avatar')
         .setDescription("Display the specified user's avatar")
-        .addUserOption(option => option.setName('user').setDescription('The user you are getting the avatar of')),
+        .addUserOption(option => option.setName('user').setDescription('The user you are getting the avatar of'))
+        .addBooleanOption(option => option.setName('absolute').setDescription('If the user has a guild avatar set and you wish to view their real avatar, use this flag')),
     async execute(client, interaction, args) {
-        const user = (await client.util.getUser(client, args['user'])) || interaction.user;
+
+        const target = await client.util.getMember(interaction.guild, args['user']) || await client.util.getUser(client, args['user']) || interaction.member;
+        const user = await client.util.getUser(client, target.id);
+        const absoluteFlagUsed = args['absolute'];
 
         const avatar = new Discord.MessageEmbed()
             .setColor(client.util.mainColor(interaction.guild))
             .setAuthor(`${user.tag}'s avatar`, client.user.displayAvatarURL())
-            .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }).replace('.webp', '.png'))
+            .setImage(target instanceof Discord.GuildMember ? absoluteFlagUsed ? target.user.displayAvatarURL({ dynamic: true, size: 1024 }).replace('.webp', '.png') : target.displayAvatarURL({ dynamic: true, size: 1024 }).replace('.webp', '.png') : target.displayAvatarURL({ dynamic: true, size: 1024 }).replace('.webp', '.png'))
             .setFooter(`Information requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL());
 
         return interaction.reply({ embeds: [avatar] });
--- commands/Information/avatar.js
+++ commands/Information/avatar.js
@@ -3,15 +3,25 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'avatar',
     description: "Displays the specified user's avatar",
-    usage: 'avatar [user]',
+    usage: 'avatar [user]\navatar [user] --absolute',
     aliases: ['av', 'icon', 'pfp'],
     async execute(client, message, args) {
-        const user = (await client.util.getUser(client, args[0])) || message.author;
+
+        let absolute = false;
+        const absoluteFlagUsed = args.find(arg => arg === '--absolute' || arg === '--a' || arg === '--user' || arg === '--u');
+
+        if (absoluteFlagUsed) {
+            absolute = true;
+            args.splice(args.indexOf(absoluteFlagUsed), 1);
+        }
+
+        const target = await client.util.getMember(message.guild, args[0]) || await client.util.getUser(client, args[0]) || message.member;
+        const user = await client.util.getUser(client, target.id);
 
         const avatar = new Discord.MessageEmbed()
             .setColor(client.util.mainColor(message.guild))
             .setAuthor(`${user.tag}'s avatar`, client.user.displayAvatarURL())
-            .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }).replace('.webp', '.png'))
+            .setImage(target instanceof Discord.GuildMember ? absoluteFlagUsed ? target.user.displayAvatarURL({ dynamic: true, size: 1024 }).replace('.webp', '.png') : target.displayAvatarURL({ dynamic: true, size: 1024 }).replace('.webp', '.png') : target.displayAvatarURL({ dynamic: true, size: 1024 }).replace('.webp', '.png'))
             .setFooter(`Information requested by ${message.author.tag}`, message.author.displayAvatarURL());
 
         return message.reply({ embeds: [avatar] });
