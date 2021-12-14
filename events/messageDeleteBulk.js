4242faff233b1091dbd7bc396543b2a58beab571
Piyerik <piyeris0@gmail.com>
2021-12-14 01:35:23 -0500

    purged message logging

---
+++ events/messageDeleteBulk.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+const settingsSchema = require('../schemas/settings-schema');
+const moment = require('moment');
+
+module.exports = {
+    name: 'messageDeleteBulk',
+    async execute(client, messages) {
+
+        const channel = messages.first().channel;
+        
+        const settings = await settingsSchema.findOne({ guildID: channel.guild.id });
+        const { messageLogging, messageLoggingIgnored } = settings;
+
+        if (!channel.guild.channels.cache.get(messageLogging) && messageLogging !== 'none') {
+            await settingsSchema.updateOne(
+                {
+                    guildID: channel.guild.id
+                },
+                {
+                    messageLogging: 'none'
+                }
+            );
+
+            client.cache.settings.delete(channel.guild.id);
+
+            return;
+        }
+
+
+        const sortedMessages = messages
+            .sort(message => +message.createdAt)
+            .map(message =>
+                `${message?.author.tag || 'Unknown'}: ${message?.content || 'No content provided'}`
+            )
+            .join('\n');
+            
+        const logEmbed = new Discord.MessageEmbed()
+            .setAuthor('Parallel Logging', client.user.displayAvatarURL())
+            .setColor(client.config.colors.log)
+            .setTitle(`Messages Purged`)
+            .addField('Channel', channel.toString(), true)
+            .addField('Purged Message Count', messages.size.toString(), true)
+            .addField('Purged Messages', sortedMessages.length >= 1024 ? await client.util.createBin(sortedMessages) : sortedMessages);
+
+
+            
+        const bin = await client.util.createBin(
+            [...messages.values()]
+                .filter(message => message.content || message.attachments.size)
+                .sort(message => +message.createdAt)
+                .map(message => 
+                    `On "${moment.utc(+message.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss A')}" from "${message.author.tag}" with ID ${message.author.id}:${message.content ? `\n- Content: ${message.content}` : ''}${message.attachments.size > 0 ? `\n- Attachments: ${message.attachments.map(attachment => attachment.url).join(' + ')}` : ''}`
+                )
+                .filter(message => message)
+                .join('\n') 
+                
+            || 'No detailed information on the purged messages could be found'
+        );
+        
+        const viewDetailed = new Discord.MessageButton().setStyle('LINK').setURL(bin).setLabel('View Detailed Log')
+        const row = new Discord.MessageActionRow().addComponents(viewDetailed);
+        return channel.send({ embeds: [logEmbed], components: [row] }).catch(() => { });
+    }
+}
--- structures/PunishmentManager.js
+++ structures/PunishmentManager.js
@@ -4,6 +4,7 @@ const automodSchema = require('../schemas/automod-schema');
 const warningSchema = require('../schemas/warning-schema');
 const punishmentSchema = require('../schemas/punishment-schema');
 const fetch = require('petitio');
+const userMap = new Map();
 
 class PunishmentManager {
     
