820049e4e8548334f92f4fa9bfa76d9c78f6aff0
Piyerik <70032196+Piyerik@users.noreply.github.com>
2022-09-21 17:26:48 -0400

    Update Utils.js

--- structures/Utils.js
+++ structures/Utils.js
@@ -294,10 +294,10 @@ class Utils {
      * @param {string} mention the mention or the ID of the channel to get
      * @returns {Discord.Role | undefined} the channel that was found
      */
-    async getChannel(guild, mention) {
+    getChannel(guild, mention) {
         if (!mention) return;
         if (mention.startsWith('<#') && mention.endsWith('>')) mention = mention.slice(2, -1);
-        return guild.channels.fetch(mention);
+        return guild.channels.cache.get(mention);
     }
 
     addMemberToCollectionPrevention(guildID, memberID) {
