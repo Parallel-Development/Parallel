c99614808c2ee16ca5478f8e8e4fab7960197717
Piyeris <piyeris0@gmail.com>
2022-12-23 12:09:49 -0500

    please redeploy this railway

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import 'dotenv/config';
 import './task-manager';
 import client from './client';
 
-console.log(process.env.TOKEN!);
 client.login(process.env.TOKEN!);
 process.on('unhandledRejection', e => console.error(e));
