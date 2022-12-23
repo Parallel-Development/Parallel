53853cf34bb9a3a52b67a94c16a4720218a9c6db
Piyeris <piyeris0@gmail.com>
2022-12-23 12:03:56 -0500

    railway being annoying

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import 'dotenv/config';
 import './task-manager';
 import client from './client';
 
+console.log(process.env.TOKEN!);
 client.login(process.env.TOKEN!);
 process.on('unhandledRejection', e => console.error(e));
