4296e741401a014cb7173e4826f1e553b6b7dd06
= <piyeris0@gmail.com>
2023-04-15 19:59:58 -0400

    fix bradens problem

--- src/listeners/buttonPress.ts
+++ src/listeners/buttonPress.ts
@@ -12,15 +12,17 @@ class ButtonPressListener extends Listener {
     const button = this.client.buttons.get(interaction.customId.split(':')[0]);
     if (!button) return interaction.reply({ content: 'Unknown modal interaction.', ephemeral: true });
 
-    await button.run(interaction)
-      .catch(e => {
-        if (typeof e !== 'string')
-          return console.error(e);
+    try {
+      await button.run(interaction)
+    } catch (e) {
+      if (typeof e !== 'string')
+        return console.error(e);
 
-        if (interaction.deferred || interaction.replied)
-          return interaction.editReply({ content: e as string });
-        else return interaction.reply({ content: e, ephemeral: true });
-      });
+      if (interaction.deferred || interaction.replied)
+        return interaction.editReply({ content: e as string });
+      else return interaction.reply({ content: e, ephemeral: true });
+    }
+  }
 }
 
 export default ButtonPressListener;
