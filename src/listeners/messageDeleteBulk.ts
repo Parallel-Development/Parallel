38fa5c394736dc98f7aa89dfc9dc0c4cf8e5148f
= <piyeris0@gmail.com>
2023-07-24 17:54:14 -0400

    lol

--- src/listeners/messageDeleteBulk.ts
+++ src/listeners/messageDeleteBulk.ts
@@ -8,8 +8,9 @@ class MessageDeleteBulkListener extends Listener {
   }
 
   async run(messages: Collection<string, Message<true>>) {
+    messages = messages.filter(msg => msg instanceof Message && msg.content?.length > 0);
     if (messages.size === 0) return false;
-    messages = messages.filter(msg => msg instanceof Message && msg.content.length > 0);
+
     const refMsg = messages.first()!;
 
     const guild = await this.client.db.guild.findUnique({
@@ -47,13 +48,13 @@ class MessageDeleteBulkListener extends Listener {
     const firstMsg = messagesArr.at(-1)!;
     let prevUser = firstMsg.author.id;
 
-    let description = `${firstMsg.member!.displayName} (${firstMsg.author.username} | ${firstMsg.author.id}):\n> ${firstMsg.content}`;
+    let description = `${firstMsg.author.username} (${firstMsg.author.id}):\n> ${firstMsg.content}`;
 
     for (let i = messagesArr.length - 2; i >= 0; i--) {
       const message = messagesArr[i];
 
       if (prevUser === message.author.id) description += `\n> ${message.content}`;
-      else description += `\n${message.member!.displayName} (${message.author.username} | ${message.author.id}):\n> ${message.content}`;
+      else description += `\n${message.author.username} (${message.author.id}):\n> ${message.content}`;
       
       prevUser = message.author.id;
     }
